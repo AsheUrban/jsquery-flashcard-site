69154ad2dda702ff42c8cb0dc4fca7d18c14ddc8
Ashe Urban <ashe@goldentongue.com>
2022-06-02 09:51:30 -0700

    Add css/styles.css and js/scripts.js.
    Co-authored-by: Derrak Richard <derrak@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
+
+
+//from derrak's project
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".definition1").toggle();
+  });
+});
